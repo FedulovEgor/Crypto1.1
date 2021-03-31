1b8bc757a24881c849cf210d6e7cf345e28fb3c9
FedulovEgor <73834810+FedulovEgor@users.noreply.github.com>
2021-03-31 19:38:14 +0300

    Последняя версия Егора

--- main.py
+++ main.py
@@ -6,17 +6,13 @@ def Eratosphen(n):
     """Генерирует решето Эратосфена"""
     a = []
 
-    # Получение списка заполненного 1
     for j in range(n + 1):
         a.append(1)
 
     j = 2
-    # Просматриваем все числа меньше n
     while j * j <= n:
-        # Если число не вычеркнуто, то оно - простое
         if a[j] == 1:
             i = j * j
-            # Вычеркиваются все числа кратные j
             while i <= n:
                 a[i] = 0
                 i = i + j
@@ -24,7 +20,6 @@ def Eratosphen(n):
 
     p = []
 
-    # Получаем список простых чисел
     for j in range(2, n + 1):
         if a[j] == 1:
             p.append(j)
@@ -41,75 +36,28 @@ def Euclid(a, b):
 
 def RabinMiller(n, r):
     """Тест Рабина-Миллера для проверки простоты числа"""
-    # b = n - 1
-    #
-    # # Получение двоичной записи числа b
-    # beta = [b % 2]
-    # b = math.floor(b / 2)
-    # while b > 0:
-    #     beta.append(b % 2)
-    #     b = math.floor(b / 2)
-    #
-    # # Повторяем метод Рабина-Миллера r раз
-    # for j in range(r):
-    #     a = random.randint(2, n - 1)  # Получаем случайное основание a
-    #     # Проверяем взаимную простоту a и n
-    #     if Euclid(a, n) > 1:
-    #         return False
-    #     # Возведение числа a в степень n-1
-    #     # с помощью метода повторного возведения
-    #     # в квадрат с использованием рекуррентного соотношения
-    #     # и проверки на нетривиальный корень из 1
-    #     d = 1
-    #     for i in range(len(beta) - 1, -1, -1):
-    #         x = d
-    #         d = d * d % n  # Получение остатка от деления на n
-    #         # Проверка на нетривиальный корень из 1
-    #         if d == 1 and x != 1 and x != n - 1:
-    #             return False
-    #         # Рекуррентное соотношение
-    #         if beta[i] == 1:
-    #             d = d * a % n
-    #     # Если НОД(a,n) не равен 1
-    #     if d != 1:
-    #         return False  # n - составное
-    # return True
     b = n - 1
-    k = -1
-    beta = []
 
-    # Получение двоичное записи числа b
-    k = k + 1
-    beta.append(b % 2)
+    beta = [b % 2]
     b = b // 2
     while b > 0:
-        k = k + 1
         beta.append(b % 2)
         b = b // 2
 
-    # Повторяем метод Рабина-Миллера r раз
     for j in range(r):
-        a = random.randint(2, n - 1)  # Получаем случайное основание a
-        # Проверяем взаимную простоту a и n
+        a = random.randint(2, n - 1)
         if Euclid(a, n) > 1:
             return False
-        # Возведение числа a в степень n-1
-        # с помощью метода повторного возведения
-        # в квадрат с использованием рекуррентного соотношения
-        # и проверки на нетривиальный корень из 1
         d = 1
-        for i in range(k, -1, -1):
+        for i in range(len(beta) - 1, -1, -1):
             x = d
-            d = d * d % n  # Получение остатка от деления на n
-            # Проверка на нетривиальный корень из 1
+            d = d * d % n
             if d == 1 and x != 1 and x != n - 1:
                 return False
-            # Рекуррентное соотношение
             if beta[i] == 1:
                 d = d * a % n
-        # Если НОД(a,n) не равен 1
         if d != 1:
-            return False  # n - составное
+            return False
     return True
 
 
@@ -117,45 +65,32 @@ def IsPrime(n):
     """Тест на проверку простоты числа с помощью решета Эратосфена, иначе тест Рабина-Миллера"""
     p = Eratosphen(810)
 
-    m = len(p)
-
-    # Сравнение сгенерированного числа n с первыми 150 простыми числами
-    for i in range(m):
-        # Проверка делится ли число n на одно из первых простых чисел
+    for i in range(len(p)):
         if n % p[i] == 0:
-            # Если сгенерированное число n является одним
-            # из первых простых чисел,
             if n == p[i]:
-                return True  # то n - простое
+                return True
             else:
-                return False  # иначе n - составное
+                return False
 
-    r = 60  # Количество повторений теста Рабина-Миллера
+    r = 60
     return RabinMiller(n, r)
 
 
 def GeneratePrime(N):
     """Генерация простого числа"""
-    # n = random.randint(2, N // 2)
-    # n = 2 * n - 1
-    # while not IsPrime(n):
-    #     n = random.randint(2, N // 2)
-    #     n = 2 * n - 1
-    # return n
-    m = N // 2  # Генерация случайного числа
-    n = random.randint(2, m)  # из интервала 2..N/2
-    n = 2 * n - 1  # Получение нечетного случайного числа
+    m = N // 2
+    n = random.randint(2, m)
+    n = 2 * n - 1
 
-    # Пока число не простое, продолжаем генерировать
-    while IsPrime(n) != True:
-        m = N // 2  # Генерация случайного числа
-        n = random.randint(2, m)  # из интервала 2..N/2
-        n = 2 * n - 1  # Получение нечетного случайного числа
+    while not IsPrime(n):
+        m = N // 2
+        n = random.randint(2, m)
+        n = 2 * n - 1
     return n
 
 
 def ExtendedEuclid(a, b):
-    """Расширенный алгоритм Евклида для нахождения НОД"""
+    """Расширенный алгоритм Евклида для нахождения НОД и линейной комбинации"""
     if not b:
         return a, 1, 0
     d, xx, yy = ExtendedEuclid(b, a % b)
@@ -166,7 +101,6 @@ def ExtendedEuclid(a, b):
 
 def GenerateKeyRSA(N):
     """Генерация ключей RSA"""
-    # TODO: Зацикливается уже на p = GeneratePrime(N)
     p = GeneratePrime(N)
     q = GeneratePrime(N)
     while p == q:
